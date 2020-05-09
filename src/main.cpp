6bd97e5ba453cc818fac91033492c91416043966
Ľubomír Skirka <lubomirskirka@gmail.com>
2020-05-09 12:11:47 +0200

    done ota update

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,191 @@
+#include <ESP8266WiFi.h>
+#include <EEPROM.h>
+#include <PubSubClient.h>
 #include <Arduino.h>
+#include <ESP8266httpUpdate.h>
 
-void setup() {
-  // put your setup code here, to run once:
+String ssid;
+String password;
+String mqtt_server;
+String mqtt_name;
+String mqtt_password;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE (50)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+
+String read_String(char add)
+{
+  int i;
+  char data[100]; //Max 100 Bytes
+  int len = 0;
+  unsigned char k;
+  k = EEPROM.read(add);
+  while (k != '\0' && len < 500) //Read until null character
+  {
+    k = EEPROM.read(add + len);
+    data[len] = k;
+    len++;
+  }
+  data[len] = '\0';
+  return String(data);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup_wifi()
+{
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  ssid = read_String(0);
+  password = read_String(30);
+  WiFi.begin(ssid.c_str(), password.c_str());
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char *topic, byte *payload, unsigned int length){
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++){
+    Serial.print((char)payload[i]);
+  }
+  Serial.print("\n");
+
+  if(strcmp(topic,"nova_skusobna_update") == 0){
+    Serial.print("update\n");
+    char *source = (char *)malloc(50);
+    char *file = (char *)malloc(50);
+
+    int k = 0;
+    for (size_t i = 0; i < length; i++,k++){
+      if (((char)payload[i]) == '/'){
+        break;
+      }
+      source[i] = ((char)payload[k]);
+    }
+    source[k] = '\0';
+    size_t i = 0;
+    for (; i < length; i++,k++){
+      file[i] = ((char)payload[k]);
+    }
+    file[i] = '\0';
+
+    Serial.printf("%s\n", source);
+    Serial.printf("%s\n", file);
+
+    t_httpUpdate_return ret = ESPhttpUpdate.update(source, 80, file);
+
+    switch (ret)
+    {
+    case HTTP_UPDATE_FAILED:
+      Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
+      break;
+    case HTTP_UPDATE_NO_UPDATES:
+      Serial.println("HTTP_UPDATE_NO_UPDATES");
+      break;
+    case HTTP_UPDATE_OK:
+      Serial.println("HTTP_UPDATE_OK");
+      ESP.restart();
+    }
+  } else{
+    Serial.print("no update\n");
+  }
+  
+    
+  
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1')
+  {
+    digitalWrite(BUILTIN_LED, LOW);
+  }
+  else
+  {
+    digitalWrite(BUILTIN_LED, HIGH);
+  }
+}
+
+void reconnect()
+{
+  // Loop until we're reconnected
+  while (!client.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (  client.connect("nova_skusobna", mqtt_name.c_str(), mqtt_password.c_str()) )
+    {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe("nova_skusobna_in");
+      client.subscribe("nova_skusobna_update");
+
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup()
+{
+  pinMode(BUILTIN_LED, OUTPUT); // Initialize the BUILTIN_LED pin as an output
+  Serial.begin(9600);
+  Serial.printf("Version 2\n");
+  EEPROM.begin(512);
+
+  mqtt_server = read_String(60);
+  mqtt_name = read_String(100);
+  mqtt_password = read_String(130);
+  setup_wifi();
+  client.setServer(mqtt_server.c_str(), 1883);
+  client.setCallback(callback);
+}
+
+void loop()
+{
+
+  if (!client.connected())
+  {
+    reconnect();
+  }
+  client.loop();
+
+  unsigned long now = millis();
+  if (now - lastMsg > 2000)
+  {
+    lastMsg = now;
+    ++value;
+    snprintf(msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish("nova_skusobna_out", msg);
+  }
 }
