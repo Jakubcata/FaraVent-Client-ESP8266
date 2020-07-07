43cffb626f80686a5dfe5bae31cb257c57cc04d5
P3ter94 <peto.frnka@gmail.com>
2020-07-07 22:39:37 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,3 @@
+echo uploading...
 curl -H  "accept: application/json" -F 'binary=@.pio/build/wemosbat_development/firmware.bin' http://admin.faravent.jakubcata.eu/api/uploadBinary?version=0.01&branch=master&board=wemosbat
+echo uploaded
