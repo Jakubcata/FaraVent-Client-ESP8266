4f203d5a374cdf66d96da5ed77f2293f891f2819
P3ter94 <peto.frnka@gmail.com>
2020-07-07 22:30:05 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-curl -H  "accept: application/json" -F 'binary=@.pio/build/nodemcuv2/firmware.bin' http://admin.faravent.jakubcata.eu/api/uploadBinary
+curl -H  "accept: application/json" -F 'binary=@.pio/build/wemosbat_development/firmware.bin' http://admin.faravent.jakubcata.eu/api/uploadBinary?version=0.01&branch=master&board=wemosbat
